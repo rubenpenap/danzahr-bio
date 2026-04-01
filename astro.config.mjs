7b870cd6b63eca7a19153f67ef7e97b3f28d33cd
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2026-03-31 21:26:25 -0300

    chore: configure github pages for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rubenpenap.github.io',
-  base: '/danzahr-bio/',
+  site: 'https://bio.danzahr.com',
+  base: '/',
 });
